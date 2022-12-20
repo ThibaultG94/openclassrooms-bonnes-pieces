8248215404326373d83e58b0832086a4e29e0341
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-12-20 11:03:08 +0100

    boucle for

--- pieces.js
+++ pieces.js
@@ -19,8 +19,41 @@ prixElement.innerText = `Prix: ${article.prix} € (${
 const categorieElement = document.createElement('p');
 categorieElement.innerText = article.categorie ?? '(aucune catégorie)';
 
+// Création des élements pour l'exercice
+const descriptionElement = document.createElement('p');
+descriptionElement.innerText =
+	article.description ?? 'Pas de description pour le moment.';
+
+const stockElement = document.createElement('p');
+stockElement.innerText = article.disponibilite
+	? 'En stock'
+	: 'Rupture de stock';
+
+// Rattachement de nos balises au DOM
 const sectionFiches = document.querySelector('.fiches');
 sectionFiches.appendChild(imageElement);
 sectionFiches.appendChild(nomElement);
 sectionFiches.appendChild(prixElement);
 sectionFiches.appendChild(categorieElement);
+
+// Ajout des éléments au DOM pour l'exercice
+sectionFiches.appendChild(descriptionElement);
+sectionFiches.appendChild(stockElement);
+
+for (let i = 0; i < pieces.length; i++) {
+	// Récupération de l'élément du DOM qui accueillera les fiches
+	const sectionFiches = document.querySelector('.fiches');
+	// Création d'une balise dédiée à une pièce automobile
+	const pieceElement = document.createElement('article');
+	// On crée l'élément img
+	const imageElement = document.createElement('img');
+	// On accède à l'indice i de la liste pieces pour configurer la source de l'image
+	imageElement.src = pieces[i].image;
+	// Idem pour le nom, le prix et la catégorie...
+
+	// On rattache la balise article à la section Fiches
+	sectionFiches.appendChild(pieceElement);
+	// On rattache l'image à pieceElemeny (la balise article)
+	pieceElement.appendChild(imageElement);
+	// Idem pour le nom, le prix et la catégorie...
+}
